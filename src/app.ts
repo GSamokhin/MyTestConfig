557a4a93bd737b7d3445a139df5d4651b526a805
German Samokhin <samokhin.german@gmail.com>
2022-12-15 22:27:59 +0200

    151222_all

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,39 @@
-import express, { Express } from "express";
+import express, { Express } from 'express';
 import { Server } from 'http';
-import { inject, injectable } from "inversify";
-import { ExeptionFilter } from "./errors/exeption.filter";
-import { ILogger } from "./logger/logger.interface";
-import { LoggerService} from "./logger/logger.service";
-import {TYPES} from "./types";
-import { UserController } from "./users/users.controller";
+import { inject, injectable } from 'inversify';
+import { ExeptionFilter } from './errors/exeption.filter';
+import { ILogger } from './logger/logger.interface';
+import { TYPES } from './types';
+import { UserController } from './users/users.controller';
 
 @injectable()
 export class App {
-    app: Express;
-    server: Server;
-    port: number;
+	app: Express;
+	server: Server;
+	port: number;
 
-    constructor(
-        @inject(TYPES.ILogger) private logger: ILogger,
-        @inject(TYPES.UserController) private usersController: UserController,
-        @inject(TYPES.ExeptionFilter) private exeptionFilter: ExeptionFilter,
-    ) {
-        this.app = express();
-        this.port = 8000;
-    }
+	constructor(
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.UserController) private usersController: UserController,
+		@inject(TYPES.ExeptionFilter) private exeptionFilter: ExeptionFilter,
+	) {
+		this.app = express();
+		this.port = 8000;
+	}
 
-    useRoutes() {
-        this.app.use('/users', this.usersController.router);
-    }
+	useRoutes() {
+		this.app.use('/users', this.usersController.router);
+	}
 
-    useExeptionFilters() {
-        this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter))
-    }
+	useExeptionFilters() {
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+	}
 
-//    public async init() {
-    public init() {
-        this.useRoutes();
-        this.useExeptionFilters();
-        this.server = this.app.listen(this.port);
-        this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
-    }
+	//    public async init() {
+	public init() {
+		this.useRoutes();
+		this.useExeptionFilters();
+		this.server = this.app.listen(this.port);
+		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
+	}
 }
